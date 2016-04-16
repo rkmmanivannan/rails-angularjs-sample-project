34412f0ce9af91ad71203517b8bf9a9250ff39a1
rkmmanivannan <rkm.manivannan@gmail.com>
2016-04-16 15:50:50 -0700

    Add devise configuration

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
 
+gem 'bower-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ db/migrate/20160416171908_add_email_contstraint_to_users.rb
@@ -0,0 +1,19 @@
+class AddEmailContstraintToUsers < ActiveRecord::Migration
+  def up
+    execute %{
+      ALTER TABLE
+        users
+      ADD CONSTRAINT
+        email_must_be_company_email
+      CHECK ( email ~* '^[^@]+@example\\.com')
+    }
+  end
+  def down
+    execute %{
+      ALTER TABLE
+        users
+      DROP CONSTRAINT
+        email_must_be_company_email  
+    }
+  end
+end
